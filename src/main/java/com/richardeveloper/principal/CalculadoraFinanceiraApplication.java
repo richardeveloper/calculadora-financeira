131da07d7513fbb576d30e0fed0df431f8e092ea
richardeveloper <richard_ar@hotmail.com>
2022-01-03 14:06:42 -0300

    Versionando projeto

---
+++ src/main/java/com/richardeveloper/principal/CalculadoraFinanceiraApplication.java
@@ -0,0 +1,13 @@
+package com.richardeveloper.principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraFinanceiraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculadoraFinanceiraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/richardeveloper/principal/CalculadoraFinanceiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richardeveloper.principal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraFinanceiraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
